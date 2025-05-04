6e8337faf61768abf7f9329e8a5aa00158aab13b
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-05-04 20:42:33 +0300

    fix

--- include/table.h
+++ include/table.h
@@ -14,8 +14,8 @@ private:
   void updateRevenue(uint32_t end_minutes);
   bool occupied_ = false;
   uint32_t total_minutes_ = 0;
-  uint32_t start_minutes_;
-  uint32_t revenue_;
+  uint32_t start_minutes_ = 0;
+  uint32_t revenue_ = 0;
   uint32_t cost_per_hour_;
 };
 
